c445108ea4c13c79dc5e3289b512a8a46b40c47e
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-02 18:23:54 +0800

    Add CustomerModel

--- app/Models/CustomerModel.php
+++ app/Models/CustomerModel.php
@@ -4,17 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CustomerModel extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
-    protected $dates = ["deleted_at"];
+    protected $table = "customers";
 
-    protected $table = "admins";
-
-    protected $fillable = ["user_id", "deleted_at"];
+    protected $fillable = ["user_id"];
 
     protected $primaryKey = "id";
 
