7232a350e736c05fb3b02a37923f28c00764b763
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-02 18:29:55 +0800

    Add register blade in customer

---
+++ resources/views/customer/register.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.guestmaster')
+
+@section('title')
+    Customer Register
+@endsection
+
+@section('content')
+    <div class="container" style="color:white;">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <h1 class="text-center">Customer Register</h1>
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <p>{{ $error }}</p>
+                        @endforeach
+                    </div>
+                @endif
+
+                <form action="{{ route('customer.register') }}" method="POST" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+
+
+                    <div class="form-group">
+                        <label for="email">Email: </label><i style="color:red">*</i>
+                        <input type="text" name="email" id="email" class="form-control">
+                        @error('email')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password: </label><i style="color:red">*</i>
+                        <input type="password" name="password" id="password" class="form-control">
+                        @error('password')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Name: </label><i style="color:red">*</i>
+                        <input type="text" name="name" id="name" class="form-control">
+                        @error('name')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="images" class="control-label">Customer Profile:</label><i style="color:red"></i>
+                        <input type="file" class="form-control" id="images" name="images">
+                        @error('images')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <input type="submit" value="Sign Up" class="btn btn-primary">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
