76e09fe28ced5665b4a395760283b6834dfcb65a
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-02 18:30:01 +0800

    Add show blade in customer

---
+++ resources/views/customer/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.adminmaster')
+
+@section('title')
+    Customer Show
+@endsection
+
+@section('content')
+    <div class="pb-20 my-2">
+        <div class="text-center">
+            <h1 class="text-5xl">
+                Show Customers
+            </h1>
+        </div>
+        @forelse ($customers as $customer)
+            <section class="flex flex-wrap justify-center gap-3 p-12 w-full">
+                <div
+                    class="max-w-sm bg-white rounded-lg border border-white-200 shadow-md dark:bg-white-800 dark:border-white-700">
+                    <img src="{{ asset('images/customer/' . $customer->images) }}" alt="I am A Pic" width="400"
+                        style="max-height: 12rem;">
+                    <div class="p-3">
+                        <h5 class="mb-2 text-2xl font-bold text-center tracking-tight">{{ $customer->name }}
+                        </h5>
+                        ID<p class="mb-2 text-lg font-bold">{{ $customer->id }}</p>
+                        Email<p class="mb-2 text-lg font-bold">{{ $customer->email }}</p>
+                    </div>
+                </div>
+            </section>
+            <a href="{{ url()->previous() }}" class="bg-gray-800 text-white font-bold p-2 mt-5 text-center"
+                role="button">Back</a>
+        @empty
+            <p>No Customer Data in the Database</p>
+        @endforelse
+        </table>
+    @endsection
