c0123b501a3ae41026839bb8c8a78192f46b6404
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-04 14:33:40 +0800

    Add search in adminheader

--- resources/views/partials/adminheader.blade.php
+++ resources/views/partials/adminheader.blade.php
@@ -100,42 +100,60 @@
                             </li>
                         </ul>
             </li>
-            </ul>
-        </div>
 
-        <div id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                @if (Auth::check())
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>
-                            {{ Auth::user()->role }}
-                            <span class="caret"></span></a>
-                    @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>
-                            Guest <span class="caret"></span></a>
-                @endif
-                <ul class="dropdown-menu" style="font-size: 1.75rem;">
-                    @if (Auth::check())
-                        <li
-                            style="padding-left: 2rem; white-space: nowrap; overflow: hidden;
-                            text-overflow: ellipsis;">
-                            <p> Welcome, {{ Auth::user()->name }}</p>
-                        </li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="{{ route('user.logout') }}">Logout</a></li>
-                    @else
-                        <li><a href="{{ route('customer.registers') }}">Customer Signup</a></li>
-                        <li><a href="{{ route('supplier.registers') }}">Supplier Signup</a></li>
-                        <li><a href="{{ route('employee.registers') }}">Employee Signup</a></li>
-                        <li><a href="{{ route('admin.registers') }}">Admin Signup</a></li>
-                        <li><a href="{{ route('user.signIn') }}">Signin</a></li>
-                    @endif
-                </ul>
-                </li>
             </ul>
+
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                    aria-expanded="false"> <i class="fa fa-search" style="padding: 0 .5rem 0 0;" aria-hidden="true"></i>
+                    Search <span class="caret"></span></a>
+                <ul class="dropdown-menu" style="font-size: 1.75rem;">
+
+                    <div>
+                        <ul class="nav navbar-nav">
+                            <li style="padding: 0 1rem;">
+                                <a href="{{ url('/user/search') }}">
+                                    Search Users
+                                </a>
+                            </li>
+                        </ul>
+            </li>
         </div>
+    </div>
+
+    <div id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+            @if (Auth::check())
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                        aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>
+                        {{ Auth::user()->role }}
+                        <span class="caret"></span></a>
+                @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                        aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>
+                        Guest <span class="caret"></span></a>
+            @endif
+            <ul class="dropdown-menu" style="font-size: 1.75rem;">
+                @if (Auth::check())
+                    <li
+                        style="padding-left: 2rem; white-space: nowrap; overflow: hidden;
+                            text-overflow: ellipsis;">
+                        <p> Welcome, {{ Auth::user()->name }}</p>
+                    </li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="{{ route('user.logout') }}">Logout</a></li>
+                @else
+                    <li><a href="{{ route('customer.registers') }}">Customer Signup</a></li>
+                    <li><a href="{{ route('supplier.registers') }}">Supplier Signup</a></li>
+                    <li><a href="{{ route('employee.registers') }}">Employee Signup</a></li>
+                    <li><a href="{{ route('admin.registers') }}">Admin Signup</a></li>
+                    <li><a href="{{ route('user.signIn') }}">Signin</a></li>
+                @endif
+            </ul>
+            </li>
+        </ul>
+    </div>
     </div>
 </nav>
