43efab8c9ea9d29350067e9b6eff4a25b44a188d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-12 12:25:06 +0800

    Fix index problem

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,48 +1,48 @@
-@extends('layouts.productmaster')
-@section('body')
- <p>
-  <a href="{{ route('product.create') }}">
-  <p class="text-center text-red-700 text-lg bg-purple-500 p-2 mx-3">
-      Add Product &rarr;
-  </p>
-</a>
-</p>
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Id</th>
-        <th scope="col">Product</th>
-        <th scope="col">Description</th>
-        {{-- <th scope="col">Image</th> --}}
-        <th scope="col">Action</th>
-       
-      </tr>
-    </thead>
-    <tbody>
-        @foreach($products as $product)
-        <tr>
+@extends('layouts.customermaster')
+@section('content')
+    <p>
+        <a href="{{ route('product.create') }}">
+            <p class="text-center text-red-700 text-lg bg-purple-500 p-2 mx-3">
+                Add Product &rarr;
+            </p>
+        </a>
+    </p>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Product</th>
+                <th scope="col">Description</th>
+                {{-- <th scope="col">Image</th> --}}
+                <th scope="col">Action</th>
 
-            <td>{{$product->id}}</td>
-            <td>{{$product->product}}</td>
-            <td>{{$product->description}}</td>
-            {{-- <td><img src="{{url($product->img_path)}}" alt="" width="150px" height="100px"></td> --}}
-            <td>
-               
-                <td><a href="{{--route('product.edit',$product->id)--}}"><i class="fas fa-edit"></i></a></td>
-                <td>
-                    <form action="{{--route('product.destroy',$product->id)--}}" method="POST">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit">
-                            <i class="fa-solid fa-trash" style="color:red"></i>
-                        </button>
-                    </form>
-                </td>
-            </td>
-           
-        </tr>
-       @endforeach
-   
-    </tbody>
-  </table>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $product)
+                <tr>
+
+                    <td>{{ $product->id }}</td>
+                    <td>{{ $product->product }}</td>
+                    <td>{{ $product->description }}</td>
+                    {{-- <td><img src="{{url($product->img_path)}}" alt="" width="150px" height="100px"></td> --}}
+                    <td>
+
+                    <td><a href="{{-- route('product.edit',$product->id) --}}"><i class="fas fa-edit"></i></a></td>
+                    <td>
+                        <form action="{{-- route('product.destroy',$product->id) --}}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit">
+                                <i class="fa-solid fa-trash" style="color:red"></i>
+                            </button>
+                        </form>
+                    </td>
+                    </td>
+
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
 @endsection
