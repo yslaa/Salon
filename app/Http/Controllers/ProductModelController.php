9fbe33e734738b60dfe2ae64a5f29ab116947c2d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-12 12:23:46 +0800

    Import view

--- app/Http/Controllers/ProductModelController.php
+++ app/Http/Controllers/ProductModelController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ProductModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class ProductModelController extends Controller
 {
