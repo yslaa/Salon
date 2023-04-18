be9b954097834b30fe87192ae13a5a5deba0923f
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-18 18:37:44 +0800

    Refactor update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
