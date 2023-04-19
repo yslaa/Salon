2867d8d24df40463f489f9ea6d87a7d5ebe2061e
yslaa <crislhan.fabila@tup.edu.ph>
2023-04-19 22:44:13 +0800

    wip

---
+++ database/migrations/2023_03_18_175424_create_customers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->string('email', 255);
+            $table->string('phone_number', 255);
+            $table->string('service', 255);
+            $table->string('images', 255)->default('example.jpg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
