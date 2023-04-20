6b5c7b511109a52dc2e5862861bbe6748b883bcc
yslaa <crislhan.fabila@tup.edu.ph>
2023-04-20 09:33:04 +0800

    Change UI Navbar in Admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,31 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#1dd323",
+        
+"secondary": "#efb1cd",
+        
+"accent": "#bc0783",
+        
+"neutral": "#2E2932",
+        
+"base-100": "#F9FAFB",
+        
+"info": "#73C3D9",
+        
+"success": "#28BD5F",
+        
+"warning": "#F9D42F",
+        
+"error": "#F35386",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
