4c13fda96239eb5a4036ba22f0c33ade2a5b98da
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-04-21 14:22:21 +0800

    Fix adminheader

--- resources/views/partials/adminheader.blade.php
+++ resources/views/partials/adminheader.blade.php
@@ -1,14 +1,14 @@
-<div class="navbar bg-secondary" style="display: flex; justify-content: center; z-index:100;">
+<div class="navbar bg-secondary">
     <div class="flex-1">
-        <a href="{{ url('/adminProfile') }}">
+        <a href="{{ url('/') }}">
             <img src="/navbar/salon.png" alt="salon" style="width: 7.5rem">
         </a>
     </div>
     <div class="flex-none" style="margin-right:2.5rem;">
-        <ul class="menu menu-horizontal" style="text-align: center;">
+        <ul class="menu menu-horizontal px-1">
             <li><a href="{{ url('/transactions') }}">Transaction</a></li>
             <li><a href="{{ url('/product') }}">Stock</a></li>
-            <li tabindex="0">
+            <li>
                 <a>
                     Charts
                     <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
@@ -43,89 +43,8 @@
                     <li> <a href="{{ url('/supplier') }}">
                             Suppliers
                         </a></li>
-
-        <div class="navbar-header" style="display: grid; justify-self: start;">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a href="{{ url('/') }}">
-                <img src="/navbar/salon.png" alt="salon" style="width: 7.5rem">
-            </a>
-        </div>
-
-        <div>
-            <ul class="nav navbar-nav">
-
-                <li style="padding: 0 1rem;">
-                    <a href="{{ url('/transactions') }}">
-                        <i class="fa fa-cart-arrow-down" style="padding: 0 .5rem 0 0;" aria-hidden="true"></i>
-                        Transaction
-                    </a>
-                </li>
-
-                <li style="padding: 0 1rem;">
-                    <a href="{{ url('/product') }}">
-                        <i class="fa fa-product-hunt" style="padding: 0 .5rem 0 0;" aria-hidden="true"></i> Stock
-                    </a>
-                </li>
-
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                        aria-expanded="false"><span style="font-size: 3rem;">&#128480;</span> Charts <span
-                            class="caret"></span></a>
-                    <ul class="dropdown-menu" style="font-size: 1.75rem;">
-                        <div>
-                            <ul class="nav navbar-nav">
-                                <li style="padding: 0 1rem;">
-                                    <a href="{{ url('/dashboard/userRole') }}">
-                                        All User Chart
-                                    </a>
-                                </li>
-                        </div>
-                    </ul>
-                </li>
-
-
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                        aria-expanded="false"> <i class="fa fa-archive" style="padding: 0 .5rem 0 0;"
-                            aria-hidden="true"></i> Tables <span class="caret"></span></a>
-                    <ul class="dropdown-menu" style="font-size: 1.75rem;">
-
-                        <div>
-                            <ul class="nav navbar-nav">
-
-
-                                <li style="padding: 0 1rem;">
-                                    <a href="{{ url('/admin') }}">
-                                        Admins
-                                    </a>
-                                </li>
-
-                                <li style="padding: 0 1rem;">
-                                    <a href="{{ url('/customer') }}">
-                                        Customers
-                                    </a>
-                                </li>
-
-                                <li style="padding: 0 1rem;">
-                                    <a href="{{ url('/employee') }}">
-                                        Employees
-                                    </a>
-                                </li>
-
-                                <li style="padding: 0 1rem;">
-                                    <a href="{{ url('/supplier') }}">
-                                        Suppliers
-                                    </a>
-                                </li>
-                            </ul>
-                </li>
-            </ul>
+                </ul>
+            </li>
 
             <li><a href="{{ url('/user/search') }}">
                     Search
@@ -134,24 +53,26 @@
             <li tabindex="0">
                 @if (Auth::check())
             <li>
-                <a href="#"><i class="fa fa-user"></i></i>
+                <a href="#"><i class="fa fa-user"></i>
                     {{ Auth::user()->role }}
                     <span class="caret"></span></a>
             @else
             <li>
-                <a href="#"></a>
-                Guest</a>
+                <a href="#">
+                    Guest</a>
                 @endif
-                <ul class="p-2 bg-base-100" >
+                <ul class="p-2 bg-base-100">
                     @if (Auth::check())
-                    <li>
-                        <P>Welcome, {{ Auth::user()->name }}</P>
-                     </li>
+                        <li>
+                            <a href="{{ url('/adminProfile') }}">
+                                Welcome, {{ Auth::user()->name }}
+                            </a>
+                        </li>
                         <li>
                             <a href="{{ url('/admin/profile/edit/{id}') }}">
                                 Update Profile
                             </a>
-                        </li>
+
                         <li><a href="{{ route('user.logout') }}">Logout</a></li>
                     @else
                         <li><a href="{{ route('customer.registers') }}">Customer Signup</a></li>
