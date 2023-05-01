78bc9ba631cd26b603fa7d0569da89f18b85f024
reden21345 <mirandilla395@gmail.com>
2023-05-01 23:44:28 +0800

    faker

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory;
+use Hash;
+use DB;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+
+        $role = ['customer', 'employee', 'admin', 'supplier'];
+
+        $image = ['rango.jpg', 'reveDay1.jpg', 'joy.jpg', 'humpty dumpty.jpg'];
+
+        foreach (range(1,10) as $index){
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => Hash::make('password'),
+                'role' => $faker->randomElement($role),
+                'images' => $faker->randomElement($image),
+                'created_at' => $faker->dateTimeThisYear()->format('Y-m-d'),
+                'updated_at' => $faker->dateTimeThisYear()->format('Y-m-d')
+            ]);
+        }
+    }
+}
