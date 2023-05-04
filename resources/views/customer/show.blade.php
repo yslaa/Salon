f08f23d086e41fe08203e7591a0cab22330d4367
yslaa <crislhan.fabila@tup.edu.ph>
2023-05-04 10:36:26 +0800

    Update Customer Blade

--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -15,8 +15,18 @@
             <section class="flex flex-wrap justify-center gap-3 p-12 w-full">
                 <div
                     class="max-w-sm bg-white rounded-lg border border-white-200 shadow-md dark:bg-white-800 dark:border-white-700">
-                    <img src="{{ asset('images/customer/' . $customer->images) }}" alt="I am A Pic" width="400"
-                        style="max-height: 12rem;">
+                    @if ($customer->images)
+                        <div class="flex justify-center items-center">
+                            @foreach (explode('|', $customer->images) as $image)
+                                <div class="avatar mx-2">
+                                    <div
+                                        class="w-full h-full rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 flex justify-center items-center">
+                                        <img src="{{ asset($image) }}" alt="I am A Pic" class="w-full h-full object-cover">
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endif
                     <div class="p-3">
                         <h5 class="mb-2 text-2xl font-bold text-center tracking-tight">{{ $customer->name }}
                         </h5>
@@ -28,7 +38,7 @@
             <a href="{{ url()->previous() }}" class="bg-gray-800 text-white font-bold p-2 mt-5 text-center"
                 role="button">Back</a>
         @empty
-            <p>No Customer Data in the Database</p>
+            <p>No customer Data in the Database</p>
         @endforelse
         </table>
     @endsection
