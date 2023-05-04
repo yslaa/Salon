930f71ef251f84c45135d341da92fcf3778334a7
yslaa <crislhan.fabila@tup.edu.ph>
2023-05-04 10:38:20 +0800

    Add some seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,30 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+use App\Models\User;
+use App\Models\AdminModel;
+use App\Models\CustomerModel;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create users
+        $users = [];
+        for ($i = 0; $i < 10; $i++) {
+            $users[] = [
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => Hash::make('password123'),
+                'role' => 'user',
+            ];
+        }
+        User::insert($users);
 
         $this->call([
             // UserSeeder::class,
